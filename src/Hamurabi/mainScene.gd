625a66219a44b7b5f791cf5e8b225b0e0e085990
pedrogfsilva <pedro.silva@sou.inteli.edu.br>
2022-03-04 09:21:09 -0300

    executáveis

--- src/Hamurabi/mainScene.gd
+++ src/Hamurabi/mainScene.gd
@@ -4,6 +4,6 @@ extends Node2D
 func _ready():
 	pass
 
-#função chamada após o nó timer terminar de esperar 8 segundos(muda para a próxima cena) 
+#função chamada após o nó timer terminar de esperar 4.5 segundos(muda para a próxima cena) 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://World.tscn")
