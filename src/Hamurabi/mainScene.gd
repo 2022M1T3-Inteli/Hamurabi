553e114bde820affe8cb796bdee0f562c29ccc6c
Alysson <99350021+alyssonorwell@users.noreply.github.com>
2022-03-03 23:01:42 -0300

    removi a versão antiga do jogo

--- src/Hamurabi/mainScene.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _ready():
-	pass
-
-#função chamada após o nó timer terminar de esperar 8 segundos(muda para a próxima cena) 
-func _on_Timer_timeout():
-	get_tree().change_scene("res://World.tscn")
