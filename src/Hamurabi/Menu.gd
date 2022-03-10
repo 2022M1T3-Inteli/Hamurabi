d657bbe1517d2e80395b9ab4396c33001fd6a0d0
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2022-03-10 19:50:03 -0300

    Menu in game
    Commit com o src
    Game story do menu in game feita, fiz testes em todos os botões e funcionam como deveriam.

---
+++ src/Hamurabi/Menu.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://World2.tscn")
+
+
+func _on_SairButton_pressed():
+	get_tree().change_scene("res://saida.tscn")
+
+
+func _on_ComoJogarButton_pressed():
+	get_tree().change_scene("res://opções.tscn")
+
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://créditos.tscn")
