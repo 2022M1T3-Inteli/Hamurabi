0b596ad9cf47f18f2f5aef65b4c0c6fc1fed989a
kathlyn diwan <kathlyn.diwan@sou.inteli.edu.br>
2022-03-10 13:59:59 -0300

    vitória
    cena final

--- src/Hamurabi/Node2D.gd
+++ src/Hamurabi/Node2D.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +15,20 @@ func _ready():
 #func _process(delta):
 #	pass
 
+
 func _on_Button_pressed():
 	get_tree().change_scene("res://World.tscn")
 	pass # Replace with function body.
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
