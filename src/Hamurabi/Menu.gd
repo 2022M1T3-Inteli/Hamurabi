bde89b4b8fb8555704f158a371d854078e25edde
Giovana Thomé <giovana.thome@sou.inteli.edu.br>
2022-03-18 21:20:26 -0300

    Testagem
    O áudio da cena de impeachment não está sendo executado em loop, está sendo interrompido.
    O áudio da cena de vitória está rodando em loop, mas temos que polir para não parecer que ele tem final.
    Menu principal está rodando corretamente, menu-in-game precisa ser arrumando pois não está mais funcionando (vi que foi comentado para não dar erro no executável).
    Foi isso que observei nessa testagem.

---
+++ src/Hamurabi/Menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# Começar o jogo ao clicar no botão
+func _on_start_button_pressed():
+	get_tree().change_scene("res://World.tscn")
+	
+func _on_start_button_button_down():
+	$Click.play()
+
+# Sair do jogo ao clicar no botão
+func _on_SairButton_pressed():
+	get_tree().change_scene("res://saida.tscn")
+
+# Abrir tutorial quando clicar no botão
+func _on_ComoJogarButton_pressed():
+	get_tree().change_scene("res://opções.tscn")
+
+# Abrir cena de créditos quando clicar no botão
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://créditos.tscn")
